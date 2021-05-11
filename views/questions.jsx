e341c0b80e875d43d6d47e49f64b3b7786d7a1f5
Logan Emmert <loganlemmert@gmail.com>
2021-05-10 23:22:22 -0600

    this time perhaps

--- views/questions.jsx
+++ views/questions.jsx
@@ -3,21 +3,21 @@ import question from '../models/questions';
 import Layout from './layout';
 import styles from '../public/stylesheets/table.module.css';
 
-function QuestionTable(props) {
+function QuestionTable(props){
     return (
       <table class={styles.table}>
-          <thead>
-              <tr>
-                <th>Question Wording</th>
-                <th>Question Answer</th>
-                <th>Question Type</th>
+          <thead class={styles.thead}>
+              <tr class={styles.tr}>
+                <th class={styles.th}>Question Wording</th>
+                <th class={styles.th}>Question Answer</th>
+                <th class={styles.th}>Question Type</th>
               </tr>
           </thead>
             {props.questions.map(question => (
-            <tbody><tr>
-                <td><strong>{question.qWording}</strong></td>
-                <td>{question.answer}</td>
-                <td>{question.roundType}</td>
+            <tbody class={styles.tbody}><tr class={styles.tr}>
+                <td class={styles.td}><strong class={styles.strong}>{question.qWording}</strong></td>
+                <td class={styles.td}>{question.answer}</td>
+                <td class={styles.td}>{question.roundType}</td>
             </tr></tbody> 
             ))}
       </table>
