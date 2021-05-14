70584ef8076afbfa9db41ddd4ec7b1a9ee2bdd76
Logan Emmert <loganlemmert@gmail.com>
2021-05-14 17:04:10 -0600

    added userType Check to add Questions

--- views/dashboard.jsx
+++ views/dashboard.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import Layout from './layout'
 
 function Dashboard(props) {
-  return (
-    <Layout title={props.title}>
-      <h1>{props.title}</h1>
-      <h2>Quiz Ninjas Sample Questions</h2>
-      <h3>Welcome, {props.user.firstName}</h3>
-      <a href="/courses/">Courses</a> | <a href="/users/profile">Profile</a> | <a href="/logout">Log out</a> | <a href="/addQuestion">Add Question</a> | <a href="/questions">Questions</a>
-    </Layout>
-  );
+  if(props.user.userType == "Quiz Master"){
+    return (
+      <Layout title={props.title}>
+        <h1>{props.title}</h1>
+        <h2>Quiz Ninjas Sample Questions</h2>
+        <h3>Welcome, {props.user.firstName}</h3>
+        <a href="/courses/">Courses</a> | <a href="/users/profile">Profile</a> | <a href="/logout">Log out</a> | <a href="/addQuestion">Add Question</a> | <a href="/questions">Questions</a>
+      </Layout>
+    );
+  }
+  else {
+    return (
+      <Layout title={props.title}>
+        <h1>{props.title}</h1>
+        <h2>Quiz Ninjas Sample Questions</h2>
+        <h3>Welcome, {props.user.firstName}</h3>
+        <a href="/courses/">Courses</a> | <a href="/users/profile">Profile</a> | <a href="/logout">Log out</a> | <a href="/questions">Questions</a>
+      </Layout>
+    );
+  }
+  ;
 }
 
 module.exports = Dashboard;
