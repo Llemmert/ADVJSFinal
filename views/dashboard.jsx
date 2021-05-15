ac818cb1d28ed5fb3e0e9f011c8402d0a60f3ba4
Logan Emmert <loganlemmert@gmail.com>
2021-05-14 18:41:12 -0600

    fixing

--- views/dashboard.jsx
+++ views/dashboard.jsx
@@ -6,14 +6,17 @@ function Dashboard(props) {
     return (
       <Layout title={props.title}>
         <Message message={[{msg: props.message}]}/>
+        <span>
+          {props}
+        </span>
         <h2> {props.message} </h2>
         <h2>Quiz Ninjas Sample Questions</h2>
         <h3>Welcome, {props.user.firstName}</h3>
         <a href="/courses/">Courses</a> |
         <a href="/users/profile">Profile</a> |
         <a href="/logout">Log out</a> |
-        {props.userType=="Quiz Master" ? <a href="/addQuestion">Add Question</a> : null}
-        | <a href="/questions">Questions</a>
+        {props.userType==="Quiz Master" ? <span><a href="/addQuestion">Add Question</a> | </span>: null}
+        <a href="/questions">Questions</a>
       </Layout>
     );
 }
